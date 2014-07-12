c4c5a5da72cc75232507836ce5e5c09ddd171428
Amar Kumar <akbism@gmail.com>
2014-07-12 22:11:04 +0530

    Update Plot1.R

--- RCode/Plot1.R
+++ RCode/Plot1.R
@@ -1,17 +1,23 @@
-# Check for the file 
+# Check for the file household_power_consumption.txt.
+# If not available, the program downloads for you.
 ROOT <- 'household_power_consumption.txt'
 sep <- '/'
-## A. Download data files
 if ( !file.exists(ROOT) ) {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"  
   download.file(fileUrl, destfile = "household_power_consumption.zip")
   unzip("household_power_consumption.zip", exdir=".")
 }
+
+# Read the data
 hhpc<-read.table("household_power_consumption.txt", header=T, sep=";",na.strings = "?", colClasses=c("factor","factor","numeric","numeric","numeric","numeric","numeric","numeric","numeric") )
+
+# Format the data and select the required data
 hhpc$Date <- as.Date(hhpc$Date, "%d/%m/%Y")
 hhpc1<-subset(hhpc, (Date=="2007-02-01" | Date=="2007-02-02"))
 hhpc1$DateTime<-paste(hhpc1$Date, hhpc1$Time)
 hhpc1$DateTime<-strptime(hhpc1$DateTime, "%Y-%m-%d %H:%M:%S")
+
+#Open png device and plot the required data
 png(filename="plot1.png", width=480, height=480)
 hist(hhpc1$Global_active_power, main="Global Active Power", xlab="Global Active Power(kilowatts)",col=2)
 dev.off()
